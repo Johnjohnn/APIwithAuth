b715211437e638db256b53acc5294aa6c0ed1bae
johnjohnn <conde2010@aol.com>
2021-01-20 18:46:20 -0500

    fixing bugs

--- queries.js
+++ queries.js
@@ -1,62 +1,82 @@
-const Pool = require("ps").Pool
+const Pool = require("pg").Pool
 const pool = new Pool({
-    user: "backend",
-    host: "localhost",
+    user: 'backend',
+    host: 'localhost',
     database: 'api',
-    password: "634712",
+    password: '634712',
     port:5432,
 })
-                                                                                     // this query will get all the countries from the DATABSE 
-
-const getCountries = (req, res)=> {
-    pool.query("SELECT * FROM countries ORDER BY id ASC", (error, results) =>{
-      if (error){
-          throw error
-      }  
+ 
+// CREATE ROUTE FUNCTION TO RETRIEVE ALL RECORDS FROM DATABASE TABLE
+const getCountries = (request, response) => {
+    pool.query('SELECT * FROM countries ORDER BY id ASC', (error, results) => {
+      if (error) {
+        throw error
+      }
       response.status(200).json(results.rows)
     })
-}
-                                                                                      // this query will get a country from the database from ID 
-const getCountryById =  (req, res) =>{
-    pool.query("SELECT * FROM countries WHERE id =$1", [id], (err, results) =>{
-        if(err){
-            throw error
-        }
-        res.status(200).json(results.rows)
-    })
-}
-const createCountry = (req , res) => {
-    const {name , capital} = req.body
-    pool.query("INSERT INTO countrires (name, capital) VALUES ($1, $2)", [name, capital], (error, results) =>{
-        if (error){
-            throw error
-        }
-        res.status(201).send("A new country has been added to the database")
-    })
-}
-                                                                                        // this query will update a country from the database
-const UpdateCountry = (req, res) =>{
-    const id = parseInt(req.params.id)
-    const {name, capital } = request.body
-    
-    pool.query(
-        "UPDATE countries SET name = $1, capita = $2 WHERE id = $3", 
-        [name, capital, id],
-        (error, results) =>{
-            if (error){
-                throw error
-            }
-            res.status(200).send("country has been updated in the database")
-        }
-    )
-}
-                                                                                         // this query will DELETE a country from the database 
-const deleteCountry = (res, res) =>{
+  }
+  
+  // CREATE ROUTE FUNCTION TO RETRIEVE A SINGLE RECORD FROM THE DATABASE
+  const getCountryById = (request, response) => {
     const id = parseInt(request.params.id)
-    pool.query("DETE FROM countries WHERE id = $1", [id], (error, results) =>{
-        if (error){
-            throw error
-        }
-        res.status(200).send(`Country deleted with ID: ${id}`)
+  
+    pool.query('SELECT * FROM countries WHERE id = $1', [id], (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
     })
-}
+  }
+  
+  // CREATE ROUTE FUNCTION TO ADD NEW RECORD INTO THE DATABASE
+  const createCountry = (request, response) => {
+    const { name, capital } = request.body
+  
+    pool.query('INSERT INTO countries (name, capital) VALUES ($1, $2)', [name, capital], (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(201).send('A new country has been added to the database')
+    })
+  }
+  
+  // CREATE ROUTE FUNCTION TO UPDATE EXISTING DATABASE RECORDS
+  
+  const updateCountry = (request, response) => {
+    const id = parseInt(request.params.id)
+    const { name, capital } = request.body
+  
+    pool.query(
+      'UPDATE countries SET name = $1, capital = $2 WHERE id = $3',
+      [name, capital, id],
+      (error, results) => {
+        if (error) {
+          throw error
+        }
+        response.status(200).send('Country has been updated in the database')
+      }
+    )
+  }
+  
+  // CREATE ROUTE FUNCTION TO DELETE A RECORD FROM THE DATABASE TABLE
+  
+  const deleteCountry = (request, response) => {
+    const id = parseInt(request.params.id)
+  
+    pool.query('DELETE FROM countries WHERE id = $1', [id], (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).send(`Country deleted with ID: ${id}`)
+    })
+  }
+  
+  module.exports = {
+    getCountries,
+    getCountryById,
+    createCountry,
+    updateCountry,
+    deleteCountry,
+  }
+  
