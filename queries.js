2638d67bbb7ed49a0324629ea954b18bf7e275c0
johnjohnn <conde2010@aol.com>
2021-01-20 11:00:50 -0500

    adding queries

--- queries.js
+++ queries.js
@@ -6,3 +6,22 @@ const pool = new Pool({
     password: "634712",
     port:5432,
 })
+
+
+const getCountires = (req, res)=> {
+    pool.query("SELECT * FROM countries ORDER BY id ASC", (error, results) =>{
+      if (error){
+          throw error
+      }  
+      response.status(200).json(results.rows)
+    })
+}
+
+const getCountryById =  (req, res) =>{
+    pool.query("SELECT * FROM countries WHERE id =$1", [id], (err, results) =>{
+        if(err){
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
